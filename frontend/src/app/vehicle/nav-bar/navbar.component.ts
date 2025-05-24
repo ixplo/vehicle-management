3742a9223ec8b2300e243898e9a64e69f722b83b
Andrei Eskov <ixplo@ya.ru>
2025-05-24 11:23:42 -0400

    apply styling on the Navbar

---
+++ frontend/src/app/vehicle/nav-bar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: false,
+    templateUrl: './navbar.component.html',
+    styleUrls: ['navbar.component.scss']
+})
+export class NavbarComponent {
+    constructor(public auth: AuthService, private router: Router) {}
+
+    isLoggedIn(): boolean {
+        return !!localStorage.getItem('token');
+    }
+    
+    logout() {
+        this.auth.logout();
+        this.router.navigate(['/login']);
+    }
+}
