daa6e0478da90c8eaa9590f143340aee31078539
Andrei Eskov <ixplo@ya.ru>
2025-06-16 10:36:09 -0400

    add env to local run

--- backend/start.test.sh
+++ backend/start.test.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 ./mvnw spring-boot:run -Dspring-boot.run.profiles=test
