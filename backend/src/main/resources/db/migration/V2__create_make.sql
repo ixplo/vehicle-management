eda6159c1d1eda1482b7202cf80dc09be44aa5cc
Andrei Eskov <ixplo@ya.ru>
2025-06-15 20:07:56 -0400

    adjust db migrations

--- backend/src/main/resources/db/migration/V2__create_make.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS make (
-                                    make_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                                    name    VARCHAR(100) NOT NULL
-);
